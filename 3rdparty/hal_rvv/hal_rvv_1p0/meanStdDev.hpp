bc49b6fbb99b3ce8e49293266ca73429aea9385e
NanQin555 <3135247064@qq.com>
2024-12-12 20:48:07 +0800

    hal_rvv: meanStdDev 8uc1 can pass opencv_test_core

--- 3rdparty/hal_rvv/hal_rvv_1p0/meanStdDev.hpp
+++ 3rdparty/hal_rvv/hal_rvv_1p0/meanStdDev.hpp
@@ -4,10 +4,8 @@
 #ifndef OPENCV_HAL_RVV_MEANSTDDEV_HPP_INCLUDED
 #define OPENCV_HAL_RVV_MEANSTDDEV_HPP_INCLUDED
 #include <riscv_vector.h>
-#include <functional>
-#include <vector>
 #include <iostream>
-#include <bitset>
+
 namespace cv { namespace cv_hal_rvv {
 #undef cv_hal_meanStdDev
 #define cv_hal_meanStdDev cv::cv_hal_rvv::meanStdDev
@@ -21,27 +19,33 @@ inline int meanStdDev_8u4c(const uchar* src_data, size_t src_step, int width, in
 
 inline int meanStdDev(const uchar* src_data, size_t src_step, int width, int height,
                              int src_type, double* mean_val, double* stddev_val, uchar* mask, size_t mask_step) {
-    if (src_type == CV_8UC1) 
-        return meanStdDev_8u1c(src_data, src_step, width, height, mean_val, stddev_val, mask, mask_step);
-    // else if (src_type == CV_8UC4)
-    //     return meanStdDev_8u4c(src_data, src_step, width, height, mean_val, stddev_val, mask, mask_step);
-    // else if (src_type == CV_32FC1)
-    //     return meanStdDev_32f1c(src_data, src_step, width, height, mean_val, stddev_val, mask, mask_step);
-    else return CV_HAL_ERROR_NOT_IMPLEMENTED;
+    switch (src_type) {
+        case CV_8UC1:
+            return meanStdDev_8u1c(src_data, src_step, width, height, mean_val, stddev_val, mask, mask_step);
+        case CV_8UC4:
+            return meanStdDev_8u4c(src_data, src_step, width, height, mean_val, stddev_val, mask, mask_step);
+        // case CV_32FC1:
+        //     return meanStdDev_32f1c(src_data, src_step, width, height, mean_val, stddev_val, mask, mask_step);
+        default:
+            return CV_HAL_ERROR_NOT_IMPLEMENTED;
+    }
 }
 
 inline int meanStdDev_8u4c(const uchar* src_data, size_t src_step, int width, int height,
                             double* mean_val, double* stddev_val, uchar* mask, size_t mask_step) {
-    double mean = 0.0, stddev = 0.0;
     for(size_t i=0; i<4; ++i) {
+        double mean = 0.0, stddev = 0.0;
         const uchar* src_c = src_data + i;
-        if(!stddev_val)  
+        if(!stddev_val)  {
             meanStdDev_8u1c(src_c, src_step, width, height, &mean, nullptr, mask, mask_step);
-        else
+            *(mean_val + i * 8) = mean;
+        }
+        else {
             meanStdDev_8u1c(src_c, src_step, width, height, &mean, &stddev, mask, mask_step);
+            *(mean_val + i * 8) = mean;
+            *(stddev_val + i * 8) = stddev;
+        }
     }
-    *mean_val = mean;
-    *stddev_val = stddev;
     return CV_HAL_ERROR_OK;
 }
 
@@ -135,7 +139,6 @@ inline int meanStdDev_8u4c(const uchar* src_data, size_t src_step, int width, in
 //     //         j += vl;
 //     //     }
 //     // }
-    
 //     for (i ; i < (size_t)height; ++i) {
 //         std::cout << "i:" << i << std::endl;
 //         const uchar* src_row = src_data + i * src_step;
@@ -143,7 +146,6 @@ inline int meanStdDev_8u4c(const uchar* src_data, size_t src_step, int width, in
 //         for (size_t j = 0 ; j < (size_t)width; j+=vl, total_count+=vl) {
 //             vl = __riscv_vsetvl_e8m1(width-j); // tail elements
 //             std::cout << "j:" << j << " vl: " << vl << std::endl;
-
 //             // Load src[row][i .. i+vl]
 //             vuint8m1_t pixel_vector = __riscv_vle8_v_u8m1(src_row + j, vl);
 //             uint8_t data[vl];
@@ -155,8 +157,7 @@ inline int meanStdDev_8u4c(const uchar* src_data, size_t src_step, int width, in
 //             std::cout  << std::endl;
 //             // vuint16m2_t pixel_squared_vector = __riscv_vmv_v_x_u16m2(0, vl);
 //             if(mask) {
-//                 // Load mask[row][i .. i+vl]
-                
+//                 // Load mask[row][i .. i+vl]              
 //                 vbool8_t mask_vector = __riscv_vlm_v_b8(mask_row + j, vl);
 //                 uint8_t maskdata[vl];
 //                 __riscv_vsm_v_b8(maskdata, mask_vector, vl);
@@ -167,7 +168,6 @@ inline int meanStdDev_8u4c(const uchar* src_data, size_t src_step, int width, in
 //                 std::cout  << std::endl;
 //                 // vec_s[0] <- sum(vec_u16m1_zero[0] , pixel_vector[*]) , if not masked
 //                 vec_s = __riscv_vwredsumu_vs_u8m1_u16m1_m(mask_vector, pixel_vector, u16_zero, vl);
-
 //                 // if(stddev_val) {
 //                 //     // pixel_squared_vector[*] = pixel_vector[*] * pixel_vector[*] , if not masked
 //                 //     pixel_squared_vector = __riscv_vwmulu_vv_u16m2_m(mask_vector, pixel_vector, pixel_vector, vl);
@@ -189,7 +189,6 @@ inline int meanStdDev_8u4c(const uchar* src_data, size_t src_step, int width, in
 //             // vuint64m4_t temp_sum = __riscv_vzext_vf4_u64m4(vec_s, vl);
 //             // uint64_t temp_sum_1 = __riscv_vmv_x_s_u64m4_u64(temp_sum);
 //             auto temp_sum = __riscv_vmv_x_s_u16m1_u16(vec_s);
-
 //             // vec_sum[0] = sum( temp_sum , vec_sum)
 //             sum += static_cast<uint64_t>(temp_sum);
 //             std::cout << "temp_sum: " << temp_sum << " sum: " << sum << " total_count: " << total_count+vl << std::endl;
@@ -202,7 +201,6 @@ inline int meanStdDev_8u4c(const uchar* src_data, size_t src_step, int width, in
 //             // }
 //         }
 //     }
-
 //     if (total_count == 0)
 //     {
 //         if (mean_val) *mean_val = 0.0;
@@ -211,7 +209,6 @@ inline int meanStdDev_8u4c(const uchar* src_data, size_t src_step, int width, in
 //     }
 //     // Return values
 //     // vfloat64m1_t float_sum = __riscv_vfcvt_f_xu_v_f64m1(vec_sum, vl);
-
 //     // double dsum = __riscv_vfmv_f_s_f64m1_f64(float_sum);
 //     std::cout << "sum: " << sum << " total_count: " << total_count << std::endl;
 //     double mean = static_cast<double>(sum) / total_count;
@@ -229,24 +226,28 @@ inline int meanStdDev_8u4c(const uchar* src_data, size_t src_step, int width, in
 // }
 
 
-// test
 inline int meanStdDev_8u1c(const uchar* src_data, size_t src_step, int width, int height,
                             double* mean_val, double* stddev_val, uchar* mask, size_t mask_step) {
-    std::cout << "mask: " ;
-    for(size_t n =0; n<64; ++n) {
-        std::cout << (bool)(mask[n]) << " ";
-    }
-    std::cout << std::endl;
+
+    // if(mask) {
+    //     std::cout << "mask: " ;
+    //     for(size_t n =0; n<64; ++n) {
+    //         std::cout << (bool)mask[n] << " ";
+    //     }
+    //     std::cout << std::endl;
+    // }
+
     // initialize variables
     size_t total_count = 0;
-    size_t vl = __riscv_vsetvlmax_e8m8();
-    uint64_t sum = 0;
-    std::cout << "height: " << height << " width: " << width << " src_step: " << src_step << " mask_step: " << mask_step << std::endl;
-    vuint64m4_t vec_sum = __riscv_vmv_v_x_u64m4(0, vl);
-    vuint64m1_t vec_sqsum = __riscv_vmv_v_x_u64m1(0, vl);
-    vuint16m1_t vec_s = __riscv_vmv_v_x_u16m1(0, vl);
+    uint64_t sum = 0, sqsum = 0;
+
+    // std::cout << "height: " << height << " width: " << width << " src_step: " << src_step << " mask_step: " << mask_step << std::endl;
+
+    size_t vl = __riscv_vsetvlmax_e8m1();
+    vuint16m1_t vec_sum = __riscv_vmv_v_x_u16m1(0, vl);
     vuint16m1_t u16_zero = __riscv_vmv_v_x_u16m1(0, vl);
-    vuint32m1_t temp_sqsum = __riscv_vmv_v_x_u32m1(0, vl);
+    vuint32m1_t vec_sqsum = __riscv_vmv_v_x_u32m1(0, vl);
+    vuint32m1_t u32_zero = __riscv_vmv_v_x_u32m1(0, vl);
     size_t i = 0;
     for (i ; i < (size_t)height; ++i) {
         std::cout << std::endl;
@@ -262,40 +263,62 @@ inline int meanStdDev_8u1c(const uchar* src_data, size_t src_step, int width, in
             // Load src[row][i .. i+vl]
             vuint8m1_t pixel_vector = __riscv_vle8_v_u8m1(src_row + j, vl);
 
-            // print pixel
-            uint8_t data[vl];
-            __riscv_vse8_v_u8m1(data, pixel_vector, vl);
-            std::cout << "pixel_vector: ";
-            for(size_t k=0; k<vl; k++) {
-                std::cout  << (int)data[k] << " ";
-            }
-            std::cout  << std::endl;
+            // // print pixel
+            // uint8_t data[vl];
+            // __riscv_vse8_v_u8m1(data, pixel_vector, vl);
+            // std::cout << "pixel_vector: ";
+            // for(size_t k=0; k<vl; k++) {
+            //     std::cout  << (int)data[k] << " ";
+            // }
+            // std::cout  << std::endl;
+
             if(mask) {
                 // Load mask[row][i .. i+vl]
-                vbool8_t mask_vector = __riscv_vlm_v_b8(mask_row + j, vl);
+                vuint8m1_t mask_load = __riscv_vle8_v_u8m1(mask_row + j, vl);
+                vbool8_t mask_vector =  __riscv_vmsne_vx_u8m1_b8(mask_load, 0, vl);
 
-                // print mask
-                uint8_t maskdata[vl];
-                __riscv_vsm_v_b8(maskdata, mask_vector, vl);
-                std::cout << "mask_vector: ";
-                for(size_t k=0; k<vl; ++k) {
-                    std::cout  << (bool)maskdata[k] << " ";
-                }
-                std::cout  << std::endl;
+                // // print mask
+                // std::cout << "mask_load: ";
+                // for(size_t k=0; k<vl; ++k) {
+                //     std::cout << (bool)mask_row[k] << " ";
+                // }
+                // std::cout << std::endl;
+                // uchar maskdata[vl];
+                // __riscv_vsm_v_b8(maskdata, mask_vector, vl);
+                // std::cout << "mask_vector: ";
+                // for(size_t k=0; k<vl; ++k) {
+                //     std::cout  << (bool)maskdata[k] << " ";
+                // }
+                // std::cout  << std::endl;
+
                 // vec_s[0] <- sum(vec_u16m1_zero[0] , pixel_vector[*]) , if not masked
-                vec_s = __riscv_vwredsumu_vs_u8m1_u16m1_m(mask_vector, pixel_vector, u16_zero, vl);
+                vec_sum = __riscv_vwredsumu_vs_u8m1_u16m1_m(mask_vector, pixel_vector, u16_zero, vl);
+                if(stddev_val) {
+                    // pixel_squared_vector[*] = pixel_vector[*] * pixel_vector[*] , if not masked
+                    auto pixel_squared_vector = __riscv_vwmulu_vv_u16m2_m(mask_vector, pixel_vector, pixel_vector, vl);
+                    // sqsum[0] <- sum(vec_u32m2_zero[0] , pixel_squared_vector[*]) , if not masked
+                    vec_sqsum = __riscv_vwredsumu_vs_u16m2_u32m1_m(mask_vector, pixel_squared_vector, u32_zero, vl);
+                }
                 total_count +=  __riscv_vcpop_m_b8(mask_vector, vl);
             }
             else {
                 // vec_s[0] <- sum(vec_u16m1_zero[0] , pixel_vector[*])
-                vec_s = __riscv_vwredsumu_vs_u8m1_u16m1(pixel_vector, u16_zero, vl);
+                vec_sum = __riscv_vwredsumu_vs_u8m1_u16m1(pixel_vector, u16_zero, vl);
+                if(stddev_val) {
+                    // pixel_squared_vector[*] = pixel_vector[*] * pixel_vector[*]
+                    auto pixel_squared_vector = __riscv_vwmulu_vv_u16m2(pixel_vector, pixel_vector, vl);
+                    // sqsum[0] <- sum(vec_u32m2_zero[0] , pixel_squared_vector[*])
+                    vec_sqsum = __riscv_vwredsumu_vs_u16m2_u32m1(pixel_squared_vector, u32_zero, vl);
+                }
                 total_count +=  vl;
             }
-            auto temp_sum = __riscv_vmv_x_s_u16m1_u16(vec_s);
+            auto temp_sum = __riscv_vmv_x_s_u16m1_u16(vec_sum);
             sum += static_cast<uint64_t>(temp_sum);
-            
-            std::cout << "temp_sum: " << temp_sum << " sum: " << sum << " total_count: " << total_count << std::endl;
-
+            if (stddev_val) {
+                auto temp_sqsum = __riscv_vmv_x_s_u32m1_u32(vec_sqsum);
+                sqsum += static_cast<uint64_t>(temp_sqsum);
+            }
+            // std::cout << "temp_sum: " << temp_sum << " sum: " << sum << " total_count: " << total_count << std::endl;
         }
     }
 
@@ -305,14 +328,20 @@ inline int meanStdDev_8u1c(const uchar* src_data, size_t src_step, int width, in
         if (stddev_val) *stddev_val = 0.0;
         return CV_HAL_ERROR_OK;
     }
+    // std::cout << "sum: " << sum << "sqsum: " << sqsum << " total_count: " << total_count << std::endl;
     // Return values
-
-    std::cout << "sum: " << sum << " total_count: " << total_count << std::endl;
     double mean = static_cast<double>(sum) / total_count;
     if (mean_val) {
         *mean_val = mean;
     }
-
+    std::cout << "mean: " << mean << std::endl;
+    if (stddev_val) {
+        double variance = std::max((sqsum / total_count) - (mean * mean), 0.0);
+        std::cout << "variance: " << variance << std::endl;
+        double stddev = std::sqrt(variance);
+        std::cout << "stddev: " << stddev << std::endl;
+        *stddev_val = stddev;
+    }
     return CV_HAL_ERROR_OK;
 }
 
